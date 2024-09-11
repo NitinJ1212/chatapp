b4a32e73b38726deba2690cafe7867a3bbf19d94
Nitin JANGRA <fsdnitin2023@gmail.com>
2024-09-11 13:48:12 +0530

    update frontend

---
+++ front_end/src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import Chat from './components/chat';
+import './App.css';
+
+const socket = io('http://localhost:5000'); // Your server-side origin
+
+const App = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // Listen for messages from the server
+    socket.on('message', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    // Clean up on component unmount
+    return () => {
+      socket.off('message');
+    };
+  }, []);
+
+  const sendMessage = () => {
+    const message = 'Hello from client!';
+    socket.emit('message', message); // Send message to the server
+  };
+
+  return (
+    <div>
+      <h1>Socket.IO Chat</h1>
+      {/* <button onClick={sendMessage}>Send Message</button>
+      <ul>
+        {messages.map((msg, index) => (
+          <li key={index}>{msg}</li>
+        ))}
+      </ul> */}
+
+      <Chat />
+    </div>
+  );
+};
+
+export default App;
---
+++ front_end/src/components/chat.js
@@ -0,0 +1,55 @@
+// Chat.js
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+// Connect to the Socket.IO server
+const socket = io('http://localhost:5000');
+const Chat = () => {
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    const [receivedMessage, setReceivedMessage] = useState([]);
+
+    useEffect(() => {
+        socket.on('message', (newMessage) => {
+            console.log(socket, socket?.id, "llllllllllllllllllllll");
+            setReceivedMessage((prevMessages) => [...prevMessages, newMessage]);
+        });
+
+        // Cleanup on component unmount
+        return () => {
+            socket.off('message');
+        };
+    }, []);
+
+    const sendMessage = () => {
+        if (message) {
+            socket.emit('message', message);
+            setMessage('');
+        }
+    };
+    return (
+        <div>
+            <h1>Chat</h1>
+            <div className='chat_div'>
+
+                {receivedMessage?.map((msg, index) => (
+                    <div className={`${socket?.id === msg.id ? "self_messages" : "received_messages"}`} key={index}>
+                        <span className="self_inner_text">{msg.data}</span>
+                    </div>
+                ))}
+
+                <div className='child_div'>
+                    <input
+                        className='input_msg'
+                        type="text"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    />
+                    <button onClick={sendMessage}>Send</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Chat;
